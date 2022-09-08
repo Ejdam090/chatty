af9cdf26f6353d684f69063399808c21377d5624
AdamKosior <a.kosior090@wp.pl>
2022-09-08 21:34:33 +0200

    init

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: `${process.env.PORT}`,
+    origin: 'http://localhost:3000',
     credentials: true,
   },
 });
